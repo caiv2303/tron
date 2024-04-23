869d47489b4196ce32ba8c31aa43404bc5a1d7f9
caiv2303 <160290076+caiv2303@users.noreply.github.com>
2024-04-23 01:44:19 +0000

    Version 2

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 bin/tron : src/main.cpp
-    g++ -o bin/tron src/main.cpp -Iinclude -lftxui-screen -lftxui-dom -lftxui-component -std=c++2a
+	g++ -o bin/tron src/main.cpp -Iinclude -lftxui-screen -lftxui-dom -lftxui-component -std=c++2a
 
 run : bin/tron
-    ./bin/tron
+	./bin/tron
--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,28 @@
 #include <thread>
 #include <string>
 #include <fstream>
+#include <Dibujo.hpp>
 
 using namespace std;
 using namespace ftxui;
 
 int main (int argc, char const *argv[])
 {
+    Dibujo moto("./assets/images/moto.txt");
+    Dibujo bloque("./assets/images/bloque.txt");
 
-fstream archivo;
-archivo.open("./assets/images/moto.txt");
-
-string linea;
-Element elementos;
-while (getline(archivo,linea))
-{
-    elementos.emplace_back(text(linea));    
-}
-archivo.close();
-int fotograma=0;
-while(true){
-    this_thread::sleep_for(0.1s);
-    fotograma++;
-    Element personaje = spinner (21,fotograma)| bold | color(Color::Magenta3) | bgcolor(Color::Black);
-    Element eMoto = text(moto);
-    Element dibujo = hbox({personaje, eMoto});
-    Screen pantalla= Screen::Create(Dimension::Full());
-    Render(pantalla,dibujo);
-    pantalla.Print();
-    cout<<pantalla.ResetPosition();
-}
+    int fotograma=0;
+    while(true)
+    {
+        this_thread::sleep_for(0.1s);
+        fotograma++;
+        Element personaje = spinner (21,fotograma)| bold | color(Color::Purple) | bgcolor(Color::White);
+        Element dibujo = hbox({personaje, moto.GetElement(), bloque.GetElement()});
+        Screen pantalla= Screen::Create(Dimension::Full());
+        Render(pantalla,dibujo);
+        pantalla.Print();
+        cout<<pantalla.ResetPosition();
+    }
     
     return EXIT_SUCCESS;
 }
